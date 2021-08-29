d4e3fa73ed2b94e4cac1434b160dd6758dcaa60a
vamsi krishna <vamsikrishna787@gmail.com>
2021-08-29 16:29:47 -0700

    about us

--- src/Components/Topmenu.js
+++ src/Components/Topmenu.js
@@ -4,9 +4,12 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Avatar from '@material-ui/core/Avatar';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -24,25 +27,56 @@ const useStyles = makeStyles((theme) => ({
 export default function Topmenu() {
   const classes = useStyles();
 
-  return (
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+
+  return <>
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton> */}
+         
 
-<Avatar alt="Remy Sharp" src="https://www.eqmagpro.com/wp-content/uploads/2019/04/Bolivia-joins-International-Solar-Alliance.jpg" className={classes.large} />
+           <Avatar alt="Remy Sharp" src="https://www.eqmagpro.com/wp-content/uploads/2019/04/Bolivia-joins-International-Solar-Alliance.jpg" className={classes.large} />
 
           <Typography variant="h6" className={classes.title}>
             Solar PV<br/>
             Research & Development
           </Typography>
-          <Button color="inherit">About Us</Button>
+          <Button color="inherit" onClick={handleClickOpen}>About Us</Button>
           <Button color="inherit">Contact Us</Button>
           <Button color="inherit">Social Media links</Button>
         </Toolbar>
       </AppBar>
     </div>
-  );
+
+
+
+    <Dialog  open={open} onClose={handleClose} aria-labelledby="draggable-dialog-title">
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          About
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+           How we Develope application
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+         
+          <Button onClick={handleClose} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+
+  </>;
 }
